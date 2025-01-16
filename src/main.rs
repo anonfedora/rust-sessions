d9b362bbd7f54e94fc0d5d23f06313639c61f61d
Akinshola <114211385+Akshola00@users.noreply.github.com>
2025-01-16 12:18:23 +0000

    feat: add operational function

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ fn main() {
 
 
 // function to encapsulate all integers
-fn intro_to_u() {
+fn intro_to_u(){
     let sum_result: u8 = sum(5, 10);
-    println!("the sum result is: {}", sum_result);   
-
-
+    let mult_result: u64 = multiply(5, 10);
+    let divide: f64 = divide(20.0, 10.2);
+    let subtr: isize = substract(20, 10);
+    
 }
+
 
 fn sum(x: u8, y: u8) -> u8 {
     x + y // implicit return
@@ -17,6 +19,19 @@ fn sum(x: u8, y: u8) -> u8 {
 }
 
 
+fn sum(x: u8, y: u8) -> u8 {
+    x + y
+}
+fn multiply(x: u64, y: u64) -> u64 {
+    x * y
+}
+fn divide(x: f64, y: f64) -> f64 {
+    let res: f64 = x / y;
+    return res
+}
+fn substract(x: isize, y: isize) -> isize {
+    x - y
+}
 // subtract
 // multiplication
 // division
