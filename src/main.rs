7282061a7ff1c0dfbeae3dd34210256d0fad2c6c
anonfedora <musaeleazar090@gmail.com>
2025-01-17 09:59:51 +0100

    feat: Assignment 2

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,74 @@
 fn main() {
-    intro_to_u();
+    println!(
+        "Is Even? {}. The sum of the numbers, {} and {}.",
+        is_even(5, 5),
+        5,
+        5,
+    );
+    println!(
+        "Is Even? {}. The sum of the numbers, {} and {}.",
+        is_even(22, 21),
+        22,
+        21,
+    );
+    intro_to_f();
+    say_name("Eleazar", "Anonefdora");
 }
 
-fn intro_to_u() {
-    let sum_result: u8 = sum(5, 10);
-    println!("The sum is {}", sum_result);
-
-    let subtract: u8 = subtract(15, 10);
-    println!("The subtraction is {}", subtract);
-
-    let divide: u8 = divide(15, 10);
-    println!("The division is {}", divide);
-
-    let multiply: u8 = multiply(15, 10);
-    println!("The multiplication is {}", multiply);
-
-    let module: u8 = modulo(15, 10);
-    println!("The modulo is {}", module);
+// is sum even?
+fn is_even(x: i32, y: i32) -> bool {
+    let z: i32 = sum_even(x, y);
+    if z % 2 == 0 {
+        true
+    } else {
+        false
+    }
 }
 
-fn sum(x: u8, y: u8) -> u8 {
+fn sum_even(x: i32, y: i32) -> i32 {
     x + y
 }
 
-fn subtract(x:u8, y: u8) -> u8 {
+// floating (arithmetic function)
+fn intro_to_f() {
+    let sum_result: f32 = sum(5.02, 10.02);
+    println!("The sum is {:?}", sum_result);
+
+    let subtract: f32 = subtract(15.22, 10.02);
+    println!("The subtraction is {:?}", subtract);
+
+    let divide: f32 = divide(15.02, 10.41);
+    println!("The division is {:?}", divide);
+
+    let multiply: f32 = multiply(15.90, 10.02);
+    println!("The multiplication is {:?}", multiply);
+
+    let module: f32 = modulo(15.12, 10.02);
+    println!("The modulo is {:?}", module);
+}
+
+fn sum(x: f32, y: f32) -> f32 {
+    x + y
+}
+
+fn subtract(x: f32, y: f32) -> f32 {
     x - y
 }
 
-fn divide(x:u8, y: u8) -> u8 {
+fn divide(x: f32, y: f32) -> f32 {
     x / y
 }
 
-fn multiply(x:u8, y: u8) -> u8 {
+fn multiply(x: f32, y: f32) -> f32 {
     x * y
 }
 
-fn modulo(x:u8, y: u8) -> u8 {
+fn modulo(x: f32, y: f32) -> f32 {
     x % y
+}
+
+// String concatenation
+fn say_name(first_name: &str, surname: &str) {
+    let full_name = format!("Hello, {} {}", first_name, surname);
+    println!("{}", full_name);
 }
