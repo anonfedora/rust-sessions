d9765f0ecea630574127b184e76ba2a19cba1f0e
“Akshola00” <akinniyishola07@gmail.com>
2025-01-17 09:28:11 +0100

    feat: implement check function for is even and is odd

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,19 @@ fn main() {
     intro_to_u();
 }
 
-
 // function to encapsulate all integers
 fn intro_to_u(){
     let sum_result: u8 = sum(5, 10);
     let mult_result: u64 = multiply(5, 10);
     let divide: f64 = divide(20.0, 10.2);
     let subtr: isize = substract(20, 10);
-    
+    let check: bool = check_func(5, 10);
+    println!("Sum: {}", sum_result);
+    println!("Multiplication: {}", mult_result);
+    println!("Division: {}", divide);
+    println!("Substraction: {}", subtr);    
+    println!("Check: {}", check);
 }
-
-
-fn sum(x: u8, y: u8) -> u8 {
-    x + y // implicit return
-//    return x + y; // explicit return
-}
-
 
 fn sum(x: u8, y: u8) -> u8 {
     x + y
@@ -32,6 +29,19 @@ fn divide(x: f64, y: f64) -> f64 {
 fn substract(x: isize, y: isize) -> isize {
     x - y
 }
+
+fn check_func(num1: u8, num2: u8) -> bool {
+    let sum_of_two_nums = sum(num1, num2);
+    if sum_of_two_nums % 2 == 0 {
+        println!("The sum of {} and {} is even", num1, num2);
+        return true;
+    } else {
+        println!("The sum of {} and {} is odd", num1, num2);
+        return false;
+    }
+}
+
+
 // subtract
 // multiplication
 // division
