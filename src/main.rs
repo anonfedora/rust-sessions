fc3e9ac838d64c47144729ee67a5614076e70d17
yunusabdul <yunusabdulmajidyunus38@gmail.com>
2025-01-28 19:02:11 +0100

    feat modularize code base

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,4 @@ fn main() {
     signed::intro_to_i();
     float::intro_to_float();
     string::strings();
-    //intro_to_u();
-    // println!("------------string handler-------------");
-    // string_handler();
-    // println!("------------number data type conversion-------------");
-    // nunber_data_type_converter();
-    // println!("------------string data type conversion-------------");
-    // string_data_type_converter();
-    // println!("------------signed number-------------");
-    // intro_to_i();
 }
-
-// fn check_func(num1: u8, num2: u8) -> bool {
-//     let sum_of_two_nums = sum(num1, num2);
-//     if sum_of_two_nums % 2 == 0 {
-//         println!("The sum of {} and {} is even", num1, num2);
-//         return true;
-//     } else {
-//         println!("The sum of {} and {} is odd", num1, num2);
-//         return false;
-//     }
-// }
