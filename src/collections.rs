35eb7f0dcd84d1cc8f4aac297d7876b04210308e
Abeeujah <abeeujah@gmail.com>
2025-02-26 18:47:17 +0100

    Common collections in Rust

---
+++ src/collections.rs
@@ -0,0 +1,17 @@
+// Given a Haystack(Vector of items), search for a needle inside.
+// Haystack of numbers, search for 47
+// Haystack of strings, search for Agent.
+
+// Given a string of characters
+// Convert it to an array of characters
+// Print all the characters
+// Convert the characters back to the given string.
+
+// Given a sentence, count the word occurences in the sentence.
+
+// Given a set of values associated to a particular person,
+// model an efficient structure that helps them store these values.
+// Keep track of all the people you store values for
+// and the values they are storing.
+// Set of values => Vec, Array, Slices, Tuples...
+// People => HashMap, Vec, HashSet...
